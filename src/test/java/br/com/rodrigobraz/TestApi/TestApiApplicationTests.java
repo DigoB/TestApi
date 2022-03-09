91aaf5b8a269ed7a3dfca57cd724df54426a52f1
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-03-09 20:53:19 -0300

    Project created

---
+++ src/test/java/br/com/rodrigobraz/TestApi/TestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodrigobraz.TestApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
