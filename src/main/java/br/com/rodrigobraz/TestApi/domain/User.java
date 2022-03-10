ad55690edf93b2ec66b1c6a69379d622203f86a5
Rodrigo Braz <braz.rodrigo94@gmail.com>
2022-03-09 21:14:15 -0300

    User class

---
+++ src/main/java/br/com/rodrigobraz/TestApi/domain/User.java
@@ -0,0 +1,36 @@
+package br.com.rodrigobraz.TestApi.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(getId(), user.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+}
